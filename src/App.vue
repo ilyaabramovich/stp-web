6604ba3aa1e08f168a39af3602c3537ca916ee48
ilyaabramovich <ilyaabramovich1998@gmail.com>
2019-02-08 14:00:00 +0800

    form refactor

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <div id="app">
     <TheHeading>Добавление вопроса</TheHeading>
     <QuestionForm
-      :on-chapter-add="addChapter"
-      :on-question-add="addQuestion"
-      :on-section-add="addSection"
-      :on-paragraph-add="addParagraph"
-      :on-unit-add="addUnit"
+      @json-generated="generateJSON"
+      @question-added="addQuestion"
+      @chapter-added="addChapter"
+      @section-added="addSection"
+      @paragraph-added="addParagraph"
+      @unit-added="addUnit"
     />
   </div>
 </template>
@@ -22,63 +23,50 @@ export default {
     QuestionForm,
     TheHeading
   },
-  data() {
-    return {};
-  },
   methods: {
-    addChapter(name) {
-      DbService.addChapter({
-        name
-      })
+    generateJSON() {
+      DbService.generateJSON()
+        .then(res => {
+          alert("JSON сгенерирован!");
+          // eslint-disable-next-line
+          console.log(res.data);
+        }) // eslint-disable-next-line
+        .catch(error => console.error(error));
+    },
+    addChapter(chapter) {
+      DbService.addChapter(chapter)
         .then(() => {
           alert("Глава добавлена!");
           // eslint-disable-next-line
         }) // eslint-disable-next-line
         .catch(error => console.error(error));
     },
-    addSection(name, chapterId) {
-      DbService.addSection({
-        name,
-        chapterId
-      })
+    addSection(section) {
+      DbService.addSection(section)
         .then(() => {
           alert("Раздел добавлен!");
           // eslint-disable-next-line
         }) // eslint-disable-next-line
         .catch(error => console.error(error));
     },
-    addParagraph(name, sectionId) {
-      DbService.addParagraph({
-        name,
-        sectionId
-      })
+    addParagraph(paragraph) {
+      DbService.addParagraph(paragraph)
         .then(() => {
           alert("Параграф добавлен!");
           // eslint-disable-next-line
         }) // eslint-disable-next-line
         .catch(error => console.error(error));
     },
-    addUnit(name, difficulty, paragraphId, hint) {
-      DbService.addUnit({
-        name,
-        difficulty,
-        paragraphId,
-        hint
-      })
+    addUnit(unit) {
+      DbService.addUnit(unit)
         .then(() => {
           alert("Задание добавлено!");
           // eslint-disable-next-line
         }) // eslint-disable-next-line
         .catch(error => console.error(error));
     },
-    addQuestion(unitId, name, hint, typeAnswer, answer) {
-      DbService.addQuestion({
-        unitId,
-        name,
-        hint,
-        typeAnswer,
-        answer
-      })
+    addQuestion(question) {
+      DbService.addQuestion(question)
         .then(() => {
           alert("Вопрос добавлен!");
           // eslint-disable-next-line
